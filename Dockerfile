48710247620b6c096098810b34a39db508b272f6
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-07-13 16:08:04 +0100

    Changed to run all in Docker.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /root
 ADD . .
 RUN pip install -r requirements.txt
 
-CMD python ~/capacity.py
+CMD python ~/run_all.py
---
+++ run_all.py
@@ -0,0 +1,7 @@
+from capacity import run as capacity_run
+from deletes import run as deletes_run
+
+
+if __name__ == "__main__":
+    capacity_run()
+    deletes_run()
