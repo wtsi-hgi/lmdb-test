764bf0a5c0b118b92f640f1d162ad7d48007e232
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-07-14 19:41:23 +0100

    Improves insight into issues with deleting data from LMDB database.

--- deletes.py
+++ deletes.py
@@ -1,7 +1,7 @@
 import shutil
 
-from _common import ONE_GB, ONE_MB, create_database, put, \
-    EXAMPLE_KEY, delete, calculate_largest_possible_entry
+from _common import ONE_GB, ONE_MB, create_database, put, EXAMPLE_KEY, delete, \
+    calculate_largest_possible_entry
 
 
 def run():
@@ -14,21 +14,34 @@ def run():
 
 def run_with_map_size(map_size):
     print("Running with map size %d" % map_size)
+
+    division = 10
+    for i in range(division):
+        proportion_of_capacity = (1.0 / division) * (i + 1)
+        run_with_map_size_and_proportion_of_capacity(map_size, proportion_of_capacity)
+
+
+def run_with_map_size_and_proportion_of_capacity(map_size, proportion_of_capacity):
     database, database_location = create_database(map_size)
 
     available_size = calculate_largest_possible_entry(database, map_size)
-    content = bytearray(int(available_size / 2))
+    content = bytearray(int(available_size * proportion_of_capacity))
+
+    print("Testing with content of %d%% capacity with a database of %d bytes"
+          % ((100 * proportion_of_capacity), map_size))
 
     for i in range(100):
         try:
             put(EXAMPLE_KEY, content, database)
-            print("Added content")
         except Exception as e:
-            print("Exception adding content on iteration %d" % (i + 1))
+            print("Exception adding content on iteration %d: %s" % (i + 1, e))
             break
-        print("Deleting previously added content")
-        deleted = delete(EXAMPLE_KEY, database)
-        assert deleted
+        try:
+            deleted = delete(EXAMPLE_KEY, database)
+            assert deleted
+        except Exception as e:
+            print("Exception deleting content on iteration %d: %s" % (i + 1, e))
+            break
 
     shutil.rmtree(database_location)
 
